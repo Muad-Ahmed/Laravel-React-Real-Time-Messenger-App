ef8860e685a3aedf991db21657c8f308555dc44f
Muad-Ahmed <muad8010@gmail.com>
2026-01-19 17:49:51 +0300

    feat: implement online presence channel with user resource mapping

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,10 +1,25 @@
 import { usePage } from "@inertiajs/react";
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { useEffect } from "react";
 
 const ChatLayout = ({ children }) => {
     const page = usePage();
     const conversations = page.props.conversations;
     const selectedConversation = page.props.selectedConversation;
+
+    useEffect(() => {
+        Echo.join("online")
+            .here((users) => {
+                console.log("here", users);
+            })
+            .joining((user) => {
+                console.log("joining", user);
+            })
+            .leaving((user) => {
+                console.log("leaving", user);
+            }).error(() => {
+                console.log('error' , error)
+            });
+    }, []);
 
     return (
         <>
