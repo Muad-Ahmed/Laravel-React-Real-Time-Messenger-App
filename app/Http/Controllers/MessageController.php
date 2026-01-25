9ff5dcd7de33c72ef9d85448c845b75c360a70a7
Muad-Ahmed <muad8010@gmail.com>
2026-01-25 15:21:57 +0300

    feat: complete message storage, file upload, and deletion logic

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\SocketMessage;
+use App\Http\Requests\StoreMessageRequest;
 use App\Http\Resources\MessageResource;
+use App\Models\Conversation;
 use App\Models\Group;
 use App\Models\Message;
+use App\Models\MessageAttachment;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class MessageController extends Controller
 {
@@ -56,5 +62,58 @@ class MessageController extends Controller
                 ->paginate(10);
         }
         return MessageResource::collection($messages);
+    }
+
+    public function store(StoreMessageRequest $request)
+    {
+        $data = $request->validated();
+        $data['sender_id'] = auth()->id();
+        $receiverId = $data['receiver_id'] ?? null;
+        $groupId = $data['group_id'] ?? null;
+
+        $files = $data['attachments'] ?? [];
+
+        $message = Message::create($data);
+
+        if ($files) {
+            foreach ($files as $file) {
+                $directory = 'attachments/' . Str::random(32);
+                Storage::makeDirectory($directory);
+
+                $model = [
+                    'message_id' => $message->id,
+                    'name' => $file->getClientOriginalName(),
+                    'mime' => $file->getClientMimeType(),
+                    'size' => $file->getSize(),
+                    'path' => $file->store($directory, 'public'),
+                ];
+                $attachment = MessageAttachment::create($model);
+                $attachments[] = $attachment;
+            }
+            $message->attachments = $attachments;
+        }
+
+        if ($receiverId) {
+            Conversation::updateConversationWithMessage($receiverId, auth()->id(), $message);
+        }
+
+        if ($groupId) {
+            Group::updateGroupWithMessage($groupId, $message);
+        }
+
+        SocketMessage::dispatch($message);
+
+        return new MessageResource($message);
+    }
+
+    public function destroy(Message $message)
+    {
+        if ($message->sender_id !== auth()->id()) {
+            return response()->json(['message' => 'Forbidden'], 403);
+        }
+
+        $message->delete();
+
+        return response('', 204);
     }
 }
