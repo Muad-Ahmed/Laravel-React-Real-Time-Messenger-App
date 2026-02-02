4d02e4e6bb39f1b882aa0b7e7b7e5522823fa283
Muad-Ahmed <muad8010@gmail.com>
2026-02-02 10:49:14 +0300

    feat: add group management controls and deletion logic

--- resources/js/Components/App/ConversationHeader.jsx
+++ resources/js/Components/App/ConversationHeader.jsx
@@ -4,6 +4,21 @@ import UserAvatar from "./UserAvatar";
 import GroupAvatar from "./GroupAvatar";
 
 const ConversationHeader = ({ selectedConversation }) => {
+    const onDeleteGroup = () => {
+        if (!window.confirm("Are you sure you want to delete this group?")) {
+            return;
+        }
+
+        axios
+            .delete(route("group.destroy", selectedConversation.id))
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
     return (
         <>
             {selectedConversation && (
@@ -30,6 +45,50 @@ const ConversationHeader = ({ selectedConversation }) => {
                             )}
                         </div>
                     </div>
+                    {/* for group owner : */}
+                    {selectedConversation.is_group && (
+                        <div className="flex gap-3">
+                            <GroupDescriptionPopover
+                                description={selectedConversation.description}
+                            />
+                            <GroupUsersPopover
+                                users={selectedConversation.users}
+                            />
+
+                            {selectedConversation.owner_id == authUser.id && (
+                                <>
+                                    <div
+                                        className="tooltip tooltip-left"
+                                        data-tip="Edit Group"
+                                    >
+                                        <button
+                                            onClick={(ev) =>
+                                                emit(
+                                                    "GroupModal.show",
+                                                    selectedConversation,
+                                                )
+                                            }
+                                            className="text-gray-400 hover:text-gray-200"
+                                        >
+                                            <PencilSquareIcon className="w-4" />
+                                        </button>
+                                    </div>
+
+                                    <div
+                                        className="tooltip tooltip-left"
+                                        data-tip="Delete Group"
+                                    >
+                                        <button
+                                            onClick={onDeleteGroup}
+                                            className="text-gray-400 hover:text-gray-200"
+                                        >
+                                            <TrashIcon className="w-4" />
+                                        </button>
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    )}
                 </div>
             )}
         </>
