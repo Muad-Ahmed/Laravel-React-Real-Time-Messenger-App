0de643f6525fe85832d62ed923bfab6953d1d04f
Muad-Ahmed <muad8010@gmail.com>
2026-02-17 10:13:53 +0300

    style: enhance ui in Checkbox component

--- resources/js/Components/Checkbox.jsx
+++ resources/js/Components/Checkbox.jsx
@@ -1,10 +1,16 @@
-export default function Checkbox({ className = '', ...props }) {
+export default function Checkbox({ className = "", ...props }) {
     return (
         <input
             {...props}
             type="checkbox"
             className={
-                'rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800 ' +
+                "w-5 h-5 rounded-md cursor-pointer transition-all duration-200 " +
+                "border-slate-300 dark:border-white/10 " +
+                "bg-white dark:bg-slate-800/50 " +
+                "text-indigo-500 shadow-sm " +
+                "focus:ring-2 focus:ring-indigo-500/30 focus:ring-offset-0 " +
+                "dark:checked:bg-indigo-500 dark:checked:border-indigo-500 " +
+                "hover:border-indigo-400 dark:hover:border-white/20 " +
                 className
             }
         />
